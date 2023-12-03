3014e4aa1afffa97ce0e27a3ca08844dbea04997
Lola <150840750+roselias@users.noreply.github.com>
2023-12-03 00:52:27 -0600

    modified methods

--- src/ManageMember.cpp
+++ src/ManageMember.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <list>
+#include <vector>
 #include "Member.h"
 
 
@@ -12,36 +12,58 @@ class ManageMember: public Member{
         std::string email;
         std::string password;
         std::string membershipStatus;
-        std::list<Member> members;
+        std::vector<Member> members;
 
     public:
         //Add a member
         void addMember(){
-            Member memb;
             cout << "Enter member ID:\n";
             cin >> id; 
-            memb.setMemberID(id);
             cout << "Enter member name:\n";
             getline(cin, name);
-            memb.setName(name);
             cout << "Enter member email:\n";
             cin >> email;
-            memb.setEmail(email);
             cout << "Enter member password:\n";
             cin >> password;
-            memb.setPassword(password);
             cout << "Enter member membeship status:\n";
             cin >> membershipStatus;
-            memb.setMembershipStatus(membershipStatus);
-                        
-            //members.push_back(memb);
+
+            //New member is added to end of members vector
+            members.push_back(Member(id, name, email, password, membershipStatus));
+            
         }
 
         void removeMember(int memberID){
+            //iterator to go through vector
+            std::vector<Member>::iterator it;
+            //sets iterator to position of searched element if found, else returns position past last elemt of vector
+            it = std::find(members.begin(), members.end(),memberID);
+            if(it == members.end()){
+                cout << "No member with" <<memberID<< "found.\n";
+            }
             
+            //Erase-Remove Idiom: remove moves elements not matching memberID to front of vector range & erase deletes the end element because it matches the memberID
+            members.erase(std::remove(members.begin(), members.end(), memberID), members.end());
+
+            cout <<"Member "<<memberID<<"was removed.\n";
         }
 
+        //still working on this method
         void editMember(int id){
-            
+            //iterator to go through vector
+            std::vector<Member>::iterator it;
+            //sets iterator to position of searched element if found, else returns position past last elemt of vector
+            it = std::find(members.begin(), members.end(),id);
+
+            //add Switch so user can select what to edit
+            //add setter after every input
+            cout << "Enter member name:\n";
+            getline(cin, name);
+            cout << "Enter member email:\n";
+            cin >> email;
+            cout << "Enter member password:\n";
+            cin >> password;
+            cout << "Enter member membeship status:\n";
+            cin >> membershipStatus;
         }
-}
+};
